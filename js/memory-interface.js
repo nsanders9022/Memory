b3e0835cb73b72045c88848123fe495c97a8128b
Caitlin Hines and Nicole Sanders <caitlin+nicole@>
2017-03-13 16:48:38 -0700

    get function to return number of divs with a specific class to work

--- js/memory-interface.js
+++ js/memory-interface.js
@@ -1,11 +1,27 @@
+function countDivs () {
+  var count = 0;
+  $("div").each(function (){
+    if($(this).hasClass("cardfront1"))
+    {
+      count++;
+    }
+  });
+  return count;
+
+}
+
+
+
 $(document).ready(function() {
   $('#1').click(function() {
-    $(this).toggleClass("cardfront1");
     $(this).toggleClass("backcards");
+    $(this).toggleClass("cardfront1");
+    console.log(countDivs());
   });
   $('#2').click(function() {
     $(this).toggleClass("cardfront1");
     $(this).toggleClass("backcards");
+    console.log(countDivs());
   });
   $('#3').click(function() {
     $(this).toggleClass("cardfront2");
@@ -23,4 +39,12 @@ $(document).ready(function() {
     $(this).toggleClass("cardfront3");
     $(this).toggleClass("backcards");
   });
+  console.log(countDivs());
+  disableClick();
 });
+
+
+
+function disableClick() {
+  $('#1').click(false);
+}
---
+++ js/memory.js
@@ -0,0 +1,22 @@
+// function countDivs () {
+//   // var counter = 0;
+//   // var divs = $("div")
+//   // console.log(divs);
+//   //
+//   // $("div").forEach(function(){
+//   //   if (this.hasClass("any"))
+//   //   {
+//   //     counter++
+//   //   }
+//   //   return counter;
+//   //
+//   // })
+//   var count = 0;
+//   $("div").each(function (){
+//     count++;
+//   });
+//   return count;
+//
+// }
+
+console.log(countDivs());
