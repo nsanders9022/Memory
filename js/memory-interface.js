0d9a99bcd7b4c65717aef7985c8e6f0b6cd1d61d
Caitlin Hines and Nicole Sanders <caitlin+nicole@>
2017-03-13 15:19:58 -0700

    flip between the front and back images of a card when clicked

---
+++ js/memory-interface.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('#1').click(function() {
+    $(this).toggleClass("cardfront1");
+    $(this).toggleClass("backcards");
+  });
+  $('#2').click(function() {
+    $(this).toggleClass("cardfront1");
+    $(this).toggleClass("backcards");
+  });
+  $('#3').click(function() {
+    $(this).toggleClass("cardfront2");
+    $(this).toggleClass("backcards");
+  });
+  $('#4').click(function() {
+    $(this).toggleClass("cardfront2");
+    $(this).toggleClass("backcards");
+  });
+  $('#5').click(function() {
+    $(this).toggleClass("cardfront3");
+    $(this).toggleClass("backcards");
+  });
+  $('#6').click(function() {
+    $(this).toggleClass("cardfront3");
+    $(this).toggleClass("backcards");
+  });
+});
